fa94fcef2d6f19124971180841dab8d92c5ae106
Gowreesan10 <77405616+Gowreesan10@users.noreply.github.com>
2021-09-26 19:58:49 +0530

    2021.9.26

--- app/src/main/java/com/dedsec_x47/trainer/MainActivity.kt
+++ app/src/main/java/com/dedsec_x47/trainer/MainActivity.kt
@@ -4,19 +4,63 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.dedsec_x47.trainer.databinding.ActivityMainBinding
+import com.facebook.CallbackManager
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.FacebookException
+
+import com.facebook.login.LoginResult
+
+import com.facebook.FacebookCallback
+
+import android.R
+import android.view.View
+
+import com.facebook.login.widget.LoginButton
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    //var callbackManager = CallbackManager.Factory.create();
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+/*
+        val EMAIL = "email"
+
+        loginButton = findViewById<View>(R.id.login_button) as LoginButton
+        loginButton.setReadPermissions(Arrays.asList(EMAIL))
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) {
+                // App code
+            }
+
+            override fun onCancel() {
+                // App code
+            }
+
+            override fun onError(exception: FacebookException) {
+                // App code
+            }
+        })*/
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        //binding.sampleText.text = stringFromJNI()
     }
 
     /**
