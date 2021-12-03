98b3b201f62d866ffa2b075cd6776091a3406efd
Karthick47 <63592766+Karthick47v2@users.noreply.github.com>
2021-12-03 19:56:02 +0530

    Update User flow

--- app/src/main/java/com/dedsec_x47/trainer/exercisePages/Strength.kt
+++ app/src/main/java/com/dedsec_x47/trainer/exercisePages/Strength.kt
@@ -1,14 +1,57 @@
 package com.dedsec_x47.trainer.exercisePages
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dedsec_x47.trainer.R
+import com.dedsec_x47.trainer.databinding.ActivityStrengthTrainingBinding
+import com.dedsec_x47.trainer.exercisePages.instructions.*
 
 class Strength : AppCompatActivity() {
+
+    private lateinit var activityStrengthTrainingBinding: ActivityStrengthTrainingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_strength_training)
-    }
 
-    //TODO: GOTO EACH INSTRUCTION PAGE WHEN SELECTED
+        activityStrengthTrainingBinding = ActivityStrengthTrainingBinding.inflate(layoutInflater)
+        setContentView(activityStrengthTrainingBinding.root)
+
+        activityStrengthTrainingBinding.cardPushUpST.setOnClickListener(){
+            val intent = Intent(this, PushUpInstr::class.java)
+            startActivity(intent)
+        }
+        activityStrengthTrainingBinding.cardSitUpST.setOnClickListener(){
+            val intent = Intent(this, SitupInst::class.java)
+            startActivity(intent)
+        }
+        activityStrengthTrainingBinding.cardBenchPressBarbellST.setOnClickListener(){
+            val intent = Intent(this, BenchPressBInst::class.java)
+            startActivity(intent)
+        }
+        activityStrengthTrainingBinding.cardBenchPressDumbbellST.setOnClickListener(){
+            val intent = Intent(this, BenchPressDInst::class.java)
+            startActivity(intent)
+        }
+        activityStrengthTrainingBinding.cardShoulderPressBarbellST.setOnClickListener(){
+            val intent = Intent(this, ShoulderPressBInst::class.java)
+            startActivity(intent)
+        }
+        activityStrengthTrainingBinding.cardShoulderPressDumbbell.setOnClickListener(){
+            val intent = Intent(this, ShoulderPressDInst::class.java)
+            startActivity(intent)
+        }
+        activityStrengthTrainingBinding.cardChinUpST.setOnClickListener(){
+            val intent = Intent(this, ChinUpInst::class.java)
+            startActivity(intent)
+        }
+        activityStrengthTrainingBinding.cardDeadLiftST.setOnClickListener(){
+            val intent = Intent(this, DeadliftInst::class.java)
+            startActivity(intent)
+        }
+        activityStrengthTrainingBinding.cardHammerCurlST.setOnClickListener(){
+            val intent = Intent(this, HammerCurlInst::class.java)
+            startActivity(intent)
+        }
+
+    }
 }
--- app/src/main/java/com/dedsec_x47/trainer/exercisePages/WeightLoss.kt
+++ app/src/main/java/com/dedsec_x47/trainer/exercisePages/WeightLoss.kt
@@ -1,14 +1,46 @@
 package com.dedsec_x47.trainer.exercisePages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dedsec_x47.trainer.R
+import com.dedsec_x47.trainer.databinding.ActivityWeightLossBinding
+import com.dedsec_x47.trainer.exercisePages.instructions.*
 
 class WeightLoss : AppCompatActivity() {
+
+    private lateinit var activityWeightLossBinding: ActivityWeightLossBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_weight_loss)
-    }
+        activityWeightLossBinding = ActivityWeightLossBinding.inflate(layoutInflater)
+        setContentView(activityWeightLossBinding.root)
 
-    //TODO: GOTO EACH INSTRUCTION PAGE WHEN SELECTED
+        activityWeightLossBinding.cardPushUpWL.setOnClickListener(){
+            val intent = Intent(this, PushUpInstr::class.java)
+            startActivity(intent)
+        }
+        activityWeightLossBinding.cardSquatWL.setOnClickListener(){
+            val intent = Intent(this, SquatInst::class.java)
+            startActivity(intent)
+        }
+        activityWeightLossBinding.cardPlankWL.setOnClickListener(){
+            val intent = Intent(this, PlankInst::class.java)
+            startActivity(intent)
+        }
+        activityWeightLossBinding.cardSplitSquatWL.setOnClickListener(){
+            val intent = Intent(this, SplitsquatInst::class.java)
+            startActivity(intent)
+        }
+        activityWeightLossBinding.cardSitUpWL.setOnClickListener(){
+            val intent = Intent(this, SitupInst::class.java)
+            startActivity(intent)
+        }
+        activityWeightLossBinding.cardChinUpWL.setOnClickListener(){
+            val intent = Intent(this, ChinUpInst::class.java)
+            startActivity(intent)
+        }
+        activityWeightLossBinding.cardLegRaiseWL.setOnClickListener(){
+            val intent = Intent(this, LegRaiseInst::class.java)
+            startActivity(intent)
+        }
+    }
 }
