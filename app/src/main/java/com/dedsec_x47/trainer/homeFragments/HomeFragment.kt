ca12d2641ac1e9173461239579d40df7db765fbf
Justy-11 <jathursh11@gmail.com>
2021-12-03 10:30:39 +0530

    card click listener, changed logout btn size

--- app/src/main/java/com/dedsec_x47/trainer/homeFragments/HomeFragment.kt
+++ app/src/main/java/com/dedsec_x47/trainer/homeFragments/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.dedsec_x47.trainer.homeFragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dedsec_x47.trainer.R
+import com.dedsec_x47.trainer.auth.LogOut
+import com.google.android.material.navigation.NavigationView
 
-class HomeFragment: Fragment() {
+class HomeFragment: Fragment(){
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,7 +23,33 @@ class HomeFragment: Fragment() {
 
         //view.findViewById<>()
         // TODO: GO TO STRENGTH.KT OR WEIGHTLOSS.KT OR MINIGAMES.KT ACCORDING TO SELECTED CARD
+        val card1 = view.findViewById<CardView>(R.id.cardWeightLoss)
+        val card2 = view.findViewById<CardView>(R.id.cardStrengthTraining)
+        val card3 = view.findViewById<CardView>(R.id.cardHIIT)
+
+        card1.setOnClickListener {
+            inflater.inflate(
+                R.layout.activity_weight_loss,
+                container,
+                false
+            )
+        }
+        card2.setOnClickListener {
+            inflater.inflate(
+                R.layout.activity_strength_training,
+                container,
+                false
+            )
+        }
+        card3.setOnClickListener {
+            inflater.inflate(
+                R.layout.activity_mini_games,
+                container,
+                false
+            )
+        }
 
         return view
     }
+
 }
