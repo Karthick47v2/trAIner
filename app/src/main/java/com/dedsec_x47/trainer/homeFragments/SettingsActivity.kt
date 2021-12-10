725bcabc3ade88fdaf5e5d83419f43d879b6f79c
Gowreesan10 <77405616+Gowreesan10@users.noreply.github.com>
2021-12-11 00:26:02 +0530

    setting code

--- app/src/main/java/com/dedsec_x47/trainer/homeFragments/SettingsActivity.kt
+++ app/src/main/java/com/dedsec_x47/trainer/homeFragments/SettingsActivity.kt
@@ -1,52 +1,69 @@
 package com.dedsec_x47.trainer.homeFragments
 
 import android.os.Bundle
-import android.widget.*
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.view.get
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dedsec_x47.trainer.R
 import com.dedsec_x47.trainer.auth.BooVariable
 import com.dedsec_x47.trainer.auth.UserDetails
-import com.google.android.material.textfield.TextInputEditText
-import kotlin.collections.HashMap
-import kotlin.collections.MutableMap
+import com.dedsec_x47.trainer.auth.userGender
+import com.dedsec_x47.trainer.databinding.ActivitySettingsBinding
 import kotlin.collections.set
 
 var isNameOrPicUpdated = BooVariable()
 
 class SettingsActivity : AppCompatActivity() {
-
+    private lateinit var activitySettingsBinding: ActivitySettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        activitySettingsBinding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_settings)
 
+        val genOp = resources.getStringArray(R.array.genderDropdown)
+        val genSpin = activitySettingsBinding.spinGender
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item, genOp
+        )
+        genSpin.adapter = adapter
+
+        genSpin.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>, view: View, position: Int,
+                id: Long
+            ) {
+                userGender = genOp[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                Toast.makeText(baseContext, UserDetails().readData("Gender"), Toast.LENGTH_SHORT).show()
+            }
+        }
+
         val userData: MutableMap<String, Any> = HashMap()
-        val name = findViewById<TextInputEditText>(R.id.tfEditName)
 
-        val age  = findViewById<TextInputEditText>(R.id.tfEditAge)
-//        val gender = findViewById<Spinner>(R.id.tfEditGender)
-        name.setText(UserDetails().readData("Name"))
-        age.setText(UserDetails().readData("Age"))
-//        gender.prompt(UserDetails().readData("Gender"))
+        activitySettingsBinding.tfEditName.setText(UserDetails().readData("Name"))
+        activitySettingsBinding.tfEditAge.setText(UserDetails().readData("Age"))
 
-        findViewById<Button>(R.id.btn1).setOnClickListener {
-            userData["Name"] = name.text.toString()
+        activitySettingsBinding.btn1.setOnClickListener {
+            userData["Name"] = activitySettingsBinding.tfEditName.text.toString()
         }
-        findViewById<Button>(R.id.btn2).setOnClickListener {
-            userData["Age"] = age.text.toString()
+        activitySettingsBinding.btn3.setOnClickListener {
+            userData["Age"] = activitySettingsBinding.tfEditAge.text.toString()
         }
-
-        findViewById<Button>(R.id.btn3).setOnClickListener{
-            //userData["Gender"] = gender.text.toString()
-
+        activitySettingsBinding.btn2.setOnClickListener {
+            userData["Gender"] = activitySettingsBinding.spinGender.selectedItem
         }
 
-        findViewById<Button>(R.id.btnUpdate).setOnClickListener {
+        activitySettingsBinding.btnUpdate.setOnClickListener {
             UserDetails().updateFireStoreData(userData)
             UserDetails().updateLocalDocument(userData)
             if (userData.containsKey("Name")) {
-                isNameUpdated.set(true)
+                isNameOrPicUpdated.set(true)
             }
             userData.clear()
         }
