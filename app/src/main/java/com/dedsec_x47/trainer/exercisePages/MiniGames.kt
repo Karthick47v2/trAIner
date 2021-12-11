d574dd4f35040cf43f8807de9eec6e13b73bb85e
Karthick47 <63592766+Karthick47v2@users.noreply.github.com>
2021-12-12 00:05:59 +0530

    Restored

--- app/src/main/java/com/dedsec_x47/trainer/exercisePages/MiniGames.kt
+++ app/src/main/java/com/dedsec_x47/trainer/exercisePages/MiniGames.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dedsec_x47.trainer.databinding.ActivityMiniGamesBinding
-import com.unity3d.player.UnityPlayerActivity
+//import com.unity3d.player.UnityPlayerActivity
 
 class MiniGames : AppCompatActivity() {
     private lateinit var activityMiniGamesBinding : ActivityMiniGamesBinding
@@ -14,8 +14,8 @@ class MiniGames : AppCompatActivity() {
         setContentView(activityMiniGamesBinding.root)
 
         activityMiniGamesBinding.cardGame1MG.setOnClickListener(){
-            val intent = Intent(this, UnityPlayerActivity::class.java)
-            startActivity(intent)
+            /*val intent = Intent(this, UnityPlayerActivity::class.java)
+            startActivity(intent)*/
         }
         activityMiniGamesBinding.cardComingSoonMG.setOnClickListener(){
 //            val intent = Intent(this, ::class.java)
