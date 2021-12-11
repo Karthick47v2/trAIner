1c54785fb992fe53708a42968e3467ef722daa6b
Karthick47 <63592766+Karthick47v2@users.noreply.github.com>
2021-12-11 23:47:45 +0530

    Update

--- app/src/main/java/com/dedsec_x47/trainer/aiTrainer/FullImageView.kt
+++ app/src/main/java/com/dedsec_x47/trainer/aiTrainer/FullImageView.kt
@@ -1,31 +1,35 @@
 package com.dedsec_x47.trainer.aiTrainer
 
 import android.content.Intent
+import android.media.Image
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.dedsec_x47.trainer.R
 
 
 class FullImageView : AppCompatActivity() {
 
-    var imageView: ImageView? = null
     //var textView: TextView? = null*/
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView( R.layout.full_image_view)
 
-        imageView = findViewById(R.id.imgFullViewPhoto)
+        var imageView: ImageView = findViewById(R.id.imgFullViewPhoto)
 
 
         var callingActivityIntent: Intent = getIntent()
 
         if(callingActivityIntent != null){
             var imgUri = callingActivityIntent.data
+            if(imgUri != null && imageView != null){
+                Glide.with(this).load(imgUri).into(imageView)
+            }
         }
         /*textView = findViewById(R.id.tvName)
         val intent = intent
